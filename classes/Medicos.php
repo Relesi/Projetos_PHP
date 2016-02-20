c0823ae5c3bcad125e7cbbd225c19281f38f90cd
Vitor Souza <root@php501.4linux.com.br>
2016-02-20 17:29:08 -0200

    Ajuste

---
+++ classes/Medicos.php
@@ -0,0 +1,17 @@
+<?php
+
+
+class Medicos{
+	
+	public $dadosDoPaciente;
+	
+	public function obtemFicha($dadosPaciente = null){
+		$this->dadosDoPaciente = $dadosPaciente;
+		
+	}
+	
+	public  function lerDadosPaciente(){
+		return $this->dadosDoPaciente;
+	}
+
+}
